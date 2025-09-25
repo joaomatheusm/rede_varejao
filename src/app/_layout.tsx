40c4050e50c40e22e74231c3d22c850ba15f2c6c
Pedro Henrique <phdmdo@gmail.com>
2025-09-25 20:52:00 -0300

    feat: adicionado lógica de sessão

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,32 @@
 import { Stack } from "expo-router";
+import { AuthProvider, useAuth } from '../contexts/AuthContext';
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
+import { router } from "expo-router";
 
-export default function MainLayout() {
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  );
+}
+
+function MainLayout() {
+  const {setAuth} = useAuth();
+
+  useEffect( () => {
+    supabase.auth.onAuthStateChange( (_event, session) => {
+      if(session){
+        setAuth(session.user);
+        router.replace("/(panel)/home/page");
+      }
+      setAuth(null);
+      router.replace('/');
+
+    })
+  }, []);
+
   return (
     // DEFINA TELAS DE NAVEGAÇÃO AQUI
     <Stack> 
